52d31a19bd1cbdd0c2555982b65d05450173c890
DrCytometer <olivertburton@gmail.com>
2025-04-13 09:01:46 +0100

    Initial commit

---
+++ R/unmix_ols.R
@@ -0,0 +1,27 @@
+# unmix_ols.r
+
+#' unmix.ols
+#'
+#' Performs spectral unmixing using ordinary least squares
+#'
+#' @param raw.data Expression data from raw fcs files. Cells in rows and detectors
+#' (raw channels) in columns. Columns should be fluorescent data only and must
+#' match the columns in spectra.
+#' @param spectra Spectral signatures of fluorophores, normalized between 0 and 1,
+#' with fluorophores in rows and detectors (channels) in columns.
+#' @return Unmixed data with cells in rows and fluorophores in columns.
+#' @export
+
+unmix.ols <- function( raw.data, spectra ) {
+
+  spectra <- t( spectra )
+
+  unmixing.matrix <- solve( crossprod( spectra ) ) %*% t( spectra )
+
+  unmixed.data <- raw.data %*% t( unmixing.matrix )
+
+  colnames( unmixed.data ) <- colnames( spectra )
+
+  return( unmixed.data )
+
+}
