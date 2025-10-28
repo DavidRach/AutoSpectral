a89a4fcf8743b00c62913b98d434ba1632ebab02
David Rach <drach@som.umaryland.edu>
2025-10-28 06:20:49 -0400

    Nice work Oliver! Starting some unit tests, working through some non-standard R formatting (likely from github page being in the same branch as the R package) that is causing the help file index to fail out.

---
+++ R/AutoSpectral-package.R
@@ -0,0 +1,9 @@
+#' @description
+#' To learn more about using the AutoSpectral package, please start by browsing the
+#' vignettes:
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,7 @@
+library(testthat)
+library(AutoSpectral)
+
+test_check("AutoSpectral")
+
+# Hi Oliver, yes, I am writing unit test while procrasting 
+# on my thesis writing
